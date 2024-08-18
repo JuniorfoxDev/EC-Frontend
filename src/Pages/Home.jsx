be61aebc7d0c03779fb086589fa81cc994409098
Vaibhav <76144239+JuniorfoxDev@users.noreply.github.com>
2024-08-18 12:00:36 +0000

    git push

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,11 +4,14 @@ import Banner from '../Components/Banner/Banner'
 import Fit from '../Components/Fit/Fit'
 import ProductCard from '../Components/ProductCard'
 const Home = () => {
+  
   return (
     <>
         <Banner/>
         <Fit/>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0'>
         <ProductCard/>
+        </div>
     </>
   )
 }
