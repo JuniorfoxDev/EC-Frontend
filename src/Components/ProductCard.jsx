e7514663d60e7eeaa0366858b4aeb2fb21bfef2a
Vaibhav <76144239+JuniorfoxDev@users.noreply.github.com>
2024-08-19 00:20:52 +0000

    git push

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -2,7 +2,7 @@ import { Link, Navigate, useNavigate } from "react-router-dom";
 
 const ProductCard = ({_id,images, name, price}) => {
     return (
-        <Link to={`/product/${_id}`} onClick={handleClick}  className="transform overflow-hidden bg-white duration-300 hover:scale-95 cursor-pointer">
+        <Link to={`/product/{_id}`} className="transform overflow-hidden bg-white duration-300 hover:scale-95 cursor-pointer">
              <img src={images[0].url} width={500} height={400} alt="" className="rounded-lg" />
              <div className="p-2 text-black/[0.9]">
              <h2 className="text-lg font-semibold">{name}</h2>
