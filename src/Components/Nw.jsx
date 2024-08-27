8bfd7a55d472e5ed8f882adb7962899319d5de6d
Vaibhav <76144239+JuniorfoxDev@users.noreply.github.com>
2024-08-27 16:15:53 +0000

    git push

--- src/Components/Nw.jsx
+++ src/Components/Nw.jsx
@@ -8,7 +8,7 @@ const Nw = () => {
       <div className='relative z-10 px-10 text-center flex flex-col items-center justify-center'>
         <h2 className=' text-2xl md:text-4xl poppins-extrabold py-2'>Get Discount for membership</h2>
         <p className='text-xs md:text-xl poppins-regular '>Every New membership will get a 40% Discount for the first purchase </p>
-        <button className='my-6 px-6 py-4 bg-white text-black rounded-2xl'>Join Now</button>
+        <button className='my-6 px-4 py-4 md:px-6 md:py-4 bg-white text-black rounded-2xl'>Join Now</button>
       </div>
     </div>
   )
