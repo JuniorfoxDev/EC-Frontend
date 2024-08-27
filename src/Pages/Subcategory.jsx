40c86799641894afbe50cae869e335a52fd5c1c9
Vaibhav <76144239+JuniorfoxDev@users.noreply.github.com>
2024-08-27 18:48:42 +0000

    git push

--- src/Pages/Subcategory.jsx
+++ src/Pages/Subcategory.jsx
@@ -1,5 +1,6 @@
 import { NavLink, useParams } from "react-router-dom"
 import { useEffect, useState } from "react";
+import ProductCard from "../Components/ProductCard";
 
 const Subcategory = () => {
     const {subcategory} = useParams();
@@ -22,7 +23,7 @@ const Subcategory = () => {
     return (
             <div className="mx-auto px-28 py-14">
                 <h1 className="text-2xl font-bold mb-4">Products in {subcategory}</h1>
-                <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+                <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
                 {loading ? (
                     <div className='flex flex-col justify-center items-center gap-5 py-4 opacity-20'>
                     <img src="https://nike-shoe-store.vercel.app/logo.svg" alt="" className='w-[60px] md:w-[80px]' />
