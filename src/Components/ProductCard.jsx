21e8b2fed58cc37619de8262b939c20e970d837b
Vaibhav <76144239+JuniorfoxDev@users.noreply.github.com>
2024-08-28 19:21:25 +0000

    git push

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -34,7 +34,7 @@ const ProductCard = ({ _id, images, name, price, subcategory }) => {
         <span className='poppins-medium text-[18px] text-gray-500'>{subcategory}</span>
         <h2 className='poppins-bold text-[xl] w-48 py-2'>{name}</h2>
         <h4 className='poppins-regular text-[14px]'>Rs.{price}.00</h4>
-        <button className='px-2 py-2 bg-black text-white rounded-full hover:bg-black[0.4] cursor-pointer absolute right-2 top-4'><FaPlus /></button>
+        <button onClick={handleCart} className='px-2 py-2 bg-black text-white rounded-full hover:bg-black[0.4] cursor-pointer absolute right-2 top-4'><FaPlus /></button>
       </div>
     </Link>
   )
