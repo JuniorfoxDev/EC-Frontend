bd796a26de9905730cca011b78b40dcff9aa1e44
Vaibhav <76144239+JuniorfoxDev@users.noreply.github.com>
2024-08-28 05:50:12 +0000

    git push

--- src/Pages/AllProduct.jsx
+++ src/Pages/AllProduct.jsx
@@ -30,7 +30,7 @@ const AllProduct = () => {
   };
 
   return (
-    <div className='h-[500px] bg-[#F5F5F5]'>
+    <div className='h-auto py-6 bg-[#F5F5F5]'>
       <div className='py-10 px-10 flex items-center justify-between'>
          <div className='flex items-center bg-white px-4 py-4 gap-4 rounded-full w-[250px]'>
            <input type="text" placeholder='Search' className='outline-none poppins-regular' />
