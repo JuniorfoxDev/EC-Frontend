af9f474d6581deb508b6cca5a250d2dc79dbc799
Vaibhav <76144239+JuniorfoxDev@users.noreply.github.com>
2024-08-28 19:55:47 +0000

    git push

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import { HiShoppingBag } from "react-icons/hi2";
 
